9140ee7dcf5163805f8accde96dee61894af6a14
shane carr <shanepcarr@gmail.com>
2019-01-07 14:20:20 -0800

    changes

---
+++ kadaneAlgorithm/sort/012Nominal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace algos.sort
+{
+    [TestClass]
+    public class n012Nominal
+    {
+        [TestMethod]
+        public void testBaseCase()
+        {
+            int[] a = { 0, 2, 1, 2, 0 };
+            int n = 5;
+
+            SortBubble(a);
+            Assert.AreEqual(0, a[0]);
+            Assert.AreEqual(0, a[1]);
+            Assert.AreEqual(1, a[2]);
+            Assert.AreEqual(2, a[3]);
+            Assert.AreEqual(2, a[4]);
+        }
+
+        private static void SortBubble(int[] a)
+        {
+            int currentLoc = 0;
+            int searchIndex = currentLoc;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+
+                for (int s = i + 1; s < a.Length; s++)
+                {
+                    if (a[i] > a[s])
+                    {
+                        int temp = a[i];
+                        a[i] = a[s];
+                        a[s] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ kadaneAlgorithm/squareRoot.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace binarySearch
+{
+    [TestClass]
+    public class binarySearch
+    {
+        [TestMethod]
+        public void squareRoot()
+        {
+            BinarySearch.Number = 64;
+            var result = BinarySearch.SquareRoot(64);
+            Assert.AreEqual(8, result);
+        }
+
+
+    }
+
+    public class BinarySearch
+    {
+        public static int Number; 
+
+        public static int SquareRoot(int high, int low = 0)
+        {
+            
+            int mid = high / 2;
+
+            if(mid * mid == Number)
+            {
+                return mid;
+            }
+
+            if (mid * mid > Number)
+            {
+                high = mid;
+            }
+            else if (mid * mid < Number)
+            {
+                low = mid;
+            }
+            else if(high == low || high < low)
+            {
+                return 0;
+            }
+
+            return SquareRoot(high, low);
+        }
+
+
+    }
+}
