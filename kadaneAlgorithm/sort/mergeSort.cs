5c6e0c81ac22b3386c810262264334d6cdc3e970
shane carr <shanepcarr@gmail.com>
2019-01-13 21:38:00 -0800

    changes

--- kadaneAlgorithm/sort/mergeSort.cs
+++ kadaneAlgorithm/sort/mergeSort.cs
@@ -3,28 +3,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace algos.sort
 {
+    /// <summary>
+    /// to do review this a bit. i liek this solution. 
+    /// bounds got a litle confusing on base case, and swapping a/b is elegant. 
+    /// </summary>
     [TestClass]
     public class MergeSort
     {
         [TestMethod]
         public void testMergeSort()
         {
-            int[] a = { 0, 2, 1, 2, 0 };
-            int[] b = new int[5];
+            int[] unsorted = { 0, 2, 1, 2, 0 };
+            int[] newArray = new int[5];
 
-            Array.Copy(a, b, a.Length);
+            Array.Copy(unsorted, newArray, unsorted.Length);
 
-            mergeSort(a, 0, a.Length-1, b);
-            Assert.AreEqual(0, b[0]);
-            Assert.AreEqual(0, b[1]);
-            Assert.AreEqual(1, b[2]);
-            Assert.AreEqual(2, b[3]);
-            Assert.AreEqual(2, b[4]);
+
+            mergeSort(unsorted, 0, unsorted.Length-1, newArray);
+            Assert.AreEqual(0, newArray[0]);
+            Assert.AreEqual(0, newArray[1]);
+            Assert.AreEqual(1, newArray[2]);
+            Assert.AreEqual(2, newArray[3]);
+            Assert.AreEqual(2, newArray[4]);
         }
 
-        public static void mergeSort(int[] a, int start, int end, int[] b)
+        public static void mergeSort(int[] b, int start, int end, int[] a)
         {
-            if( end-start < 2)
+            if( end-start <=0 )
             {
                 return;
             }
@@ -37,10 +42,10 @@ namespace algos.sort
             mergeSort(a, start, middle, b);
 
             // split right
-            mergeSort(a, middle, end, b);
+            mergeSort(a, middle+1, end, b);
 
             // merge 
-            mergeArraysBounded(a, start, middle, end, b);
+            mergeArraysBounded(b, start, middle, end, a);
 
         }
 
@@ -57,14 +62,23 @@ namespace algos.sort
             int leftPtr = start;
             int rightPtr = middle+1;
 
-            // go though each iem in array. 
-            for(int k = start; k <= end; k++)
+            ////int[] rightarray = new int[end - middle + 1];
+            ////int[] leftarray = new int[middle - start];
+            ////Array.Copy(sorted, start,leftarray, 0, middle - start);
+            ////Array.Copy(sorted,  middle+1, rightarray, 0, end - middle + 1);
+
+            // current array is start to end; . 
+            for (int k = start; k <= end; k++)
             {
                 // while there are items to consume in left. 
                 // put left in left if there are no items from the right
                 // or the left is smaller we want to take the left, otherwise we'll write the larger 
                 // value later when the right ptr is empty.
-                if (leftPtr < middle && (rightPtr > end || a[leftPtr] <= a[rightPtr]))
+                if (leftPtr <= middle && 
+                    // out of right items need to take left for the rest, it is already sorted
+                    (rightPtr > end 
+                    // compare left and right.
+                    ||  a[leftPtr] <= a[rightPtr]))
                 {
                     b[k] = a[leftPtr];
                     leftPtr++;
@@ -77,13 +91,5 @@ namespace algos.sort
                 }
             }
         }
-
-        ////public copyAToBByIndex(int[] a, int[] b, int start, int end)
-        ////{
-        ////    for(int i =start; i < end; i++)
-        ////    {
-        ////        b[i] = a[i];
-        ////    }
-        ////}
     }
 }
