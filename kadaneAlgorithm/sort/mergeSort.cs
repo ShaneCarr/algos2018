b34a12d59fe337560d2285f1184f2a42c6d1f288
shane carr <shanepcarr@gmail.com>
2019-01-13 18:21:27 -0800

    ms

--- kadaneAlgorithm/sort/mergeSort.cs
+++ kadaneAlgorithm/sort/mergeSort.cs
@@ -12,48 +12,59 @@ namespace algos.sort
             int[] a = { 0, 2, 1, 2, 0 };
             int[] b = new int[5];
 
-            mergeSorts(a, 0, a.Length, b);
-            Assert.AreEqual(0, a[0]);
-            Assert.AreEqual(0, a[1]);
-            Assert.AreEqual(1, a[2]);
-            Assert.AreEqual(2, a[3]);
-            Assert.AreEqual(2, a[4]);
+            Array.Copy(a, b, a.Length);
+
+            mergeSort(a, 0, a.Length-1, b);
+            Assert.AreEqual(0, b[0]);
+            Assert.AreEqual(0, b[1]);
+            Assert.AreEqual(1, b[2]);
+            Assert.AreEqual(2, b[3]);
+            Assert.AreEqual(2, b[4]);
         }
 
-        public static void mergeSorts(int[] a, int start, int end, int[] b)
+        public static void mergeSort(int[] a, int start, int end, int[] b)
         {
-            if(a.Length < 2)
+            if( end-start < 2)
             {
                 return;
             }
 
-            int middle = a.Length / 2;
-
+            int middle = (start +end) / 2;
+            // 2=s,4=e = 3=m
             // first split:
 
             // split left
-            mergeSorts(a, start, middle, b);
+            mergeSort(a, start, middle, b);
 
             // split right
-            mergeSorts(a, middle + 1, end, b);
+            mergeSort(a, middle, end, b);
 
             // merge 
-            merges(a, start, middle, end, b);
+            mergeArraysBounded(a, start, middle, end, b);
 
         }
 
-        public static void merges(int [] a, int start, int middle, int end, int[] b)
+        /// <summary>
+        /// merges two arrays defined by bounds start middle and middle end.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="start"></param>
+        /// <param name="middle"></param>
+        /// <param name="end"></param>
+        /// <param name="b"></param>
+        public static void mergeArraysBounded(int [] a, int start, int middle, int end, int[] b)
         {
             int leftPtr = start;
-            int rightPtr = middle;
+            int rightPtr = middle+1;
 
             // go though each iem in array. 
-            for(int k = start; k < end; k++)
+            for(int k = start; k <= end; k++)
             {
                 // while there are items to consume in left. 
-                // put in left if there are no items from the right
-                // or the left is smaller
-                if (leftPtr < middle && (rightPtr >= end || a[leftPtr] <= b[rightPtr]))
+                // put left in left if there are no items from the right
+                // or the left is smaller we want to take the left, otherwise we'll write the larger 
+                // value later when the right ptr is empty.
+                if (leftPtr < middle && (rightPtr > end || a[leftPtr] <= a[rightPtr]))
                 {
                     b[k] = a[leftPtr];
                     leftPtr++;
