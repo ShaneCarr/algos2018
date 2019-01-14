cb1411e4795bee9b0aa2ad53102347d0fff2d6ea
shane carr <shanepcarr@gmail.com>
2019-01-14 15:50:07 -0800

    changes

---
+++ kadaneAlgorithm/puzzle/findTwoNumbersThatEqualASumInListOfNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kadaneAlgorithm.puzzle
+{
+    // https://www.youtube.com/watch?v=XKu_SEDAykw&t=1218s
+    public class findTwoNumbersThatEqualASumInListOfNumbers
+    {
+        // example: [1,2,3,9] [1,2,4,4]  sum we want is 9. Items ordered. 
+    }
+}
