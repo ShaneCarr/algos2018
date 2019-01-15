8f3d8d89541b0d18cedf5c8d6c38fe27b639be11
shane carr <shanepcarr@gmail.com>
2019-01-14 19:33:32 -0800

    changes

---
+++ kadaneAlgorithm/algorithm/peakfinding/peakfinding.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace peakfinding
+{
+    /// <summary>
+    /// peak is an item where 
+    /// bigger than left or right
+    /// if end and bigger than left than peak
+    /// if beginnign and bigger than right than peak
+    /// if all same peak (
+    /// 
+    /// values don't need to be sorted.
+    /// </summary>
+    [TestClass]
+    public class binarySearch
+    {
+        [TestMethod]
+        public void squareRoot()
+        {
+            // only 9 is peak
+            int[] values = { 1, 9, 7, 6, 5 };
+            var result = BinarySearch.FindPeak(values, values.Length - 1, 0);
+            Assert.AreEqual(result, result);
+        }
+    }
+
+    public class BinarySearch
+    {
+        public static int FindPeak(int[] values, int high, int low)
+        {
+            if (values.Length == 0)
+            {
+                // throw?
+                return 0;
+            }
+
+            // Items was not found
+            if (high <= low)
+            {
+                return 0;
+            }
+
+            int mid = (high + low) / 2;
+
+            // possible peaks.
+            if (values.Length == 1)
+            {
+                return values[0];
+            }
+            else if ( mid == 0 && values[mid] > values[mid + 1]
+                // beginning; no erro already bigger than one
+                ||
+
+                // end no erro already bigger than one
+                mid == values.Length - 1 && values[mid] >= values[mid - 1] ||
+
+                // in array
+                (mid != values.Length - 1 && values[mid] >= values[mid + 1] ) && (mid != 0 && values[mid] >= values[mid - 1]))
+            {
+                return values[mid];
+            }
+
+            
+            // right is bigger.
+            if (values[mid] < values[mid +1])
+            {
+                return FindPeak(values, high, mid);
+            }
+
+            return FindPeak(values, mid, low);
+        }
+    }
+}
--- kadaneAlgorithm/algorithm/search/binarySearch.cs
+++ kadaneAlgorithm/algorithm/search/binarySearch.cs
@@ -1,12 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace kadaneAlgorithm.search
+namespace binarySearch2
 {
-    class binarySearch
+    // classic sorted list.
+    [TestClass]
+    public class binarySearch
     {
+        [TestMethod]
+        public void squareRoot()
+        {
+            int[] values = { 1, 2, 3, 4, 5 };
+            int key = 3;
+            var result = BinarySearch.Search(values, key, values.Length -1, 0);
+            Assert.AreEqual(key, result);
+        }
+
+
+    }
+
+    public class BinarySearch
+    {
+        public static int Search(int[] values, int key, int high, int low)
+        {
+
+            int mid = (high + low) / 2;
+
+            if (values[mid] == values[key])
+            {
+                return key;
+            }
+
+            if (mid  > key)
+            {
+                high = mid;
+            }
+            else if (mid < key)
+            {
+                low = mid;
+            }
+
+            // Items was not found
+            else if (high == low || high < low)
+            {
+                return 0;
+            }
+
+            return Search(values, key, high, low);
+        }
+
+
     }
 }
