21b3be39fd9711c9e68e7d522453908a8017006b
shane carr <shanepcarr@gmail.com>
2019-03-24 17:35:07 -0700

    changes

--- encoder/TestProject/UnitTest1.cs
+++ encoder/TestProject/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace TestProject
             string input = "abc10[a]";
             string outputExpected    = "abcaaaaaaaaaa";
 
-            string outputActual = decodeEncoded(0, input.Length, input.ToCharArray());
+            string outputActual = decodeEncoded(0, input.Length -1, input.ToCharArray());
 
             Assert.AreEqual(outputExpected, outputActual);
         }
@@ -20,15 +20,15 @@ namespace TestProject
 
         public static string decodeEncoded(int start, int end, char[] encoded)
         {
-            if(start == end)
-            {
-                return string.Empty;
-            }
-
+            //if(start == end)
+            //{
+            //    return string.Empty;
+            //}
+            
             // special brace character
             const char sP = '[';
             const char eP = ']';
-
+            string timesToDuplicate = "";
             // currently in between startP and endP
             bool inBracketCollecting = false;
 
@@ -49,7 +49,7 @@ namespace TestProject
                     // [a] point to a. after FIRST [
                     if (!inBracketCollecting)
                     {
-                        sInBracketSubstring = currIndex++;
+                        sInBracketSubstring = currIndex + 1;
                         inBracketCollecting = true;
                     }
 
@@ -64,13 +64,16 @@ namespace TestProject
                     if (parenCount == 0)
                     {
                         // [a] point to a. after last]
-                        eInBracketSubstring = currIndex--;
+                        eInBracketSubstring = currIndex - 1;
 
                         // encoded => <content> the indexs are first and last of same.
                         expandEncoded = decodeEncoded(sInBracketSubstring, eInBracketSubstring, encoded);
 
-                        int encodeTimes = GetNumberOfTimesToDup(encoded, sInBracketSubstring, start);
-                        
+                        int encodeTimes = 0; //GetNumberOfTimesToDup(encoded, sInBracketSubstring, start);
+                        if(!string.IsNullOrWhiteSpace(timesToDuplicate))
+                        {
+                            encodeTimes = Convert.ToInt32(timesToDuplicate);
+                        }
 
                         // clone result in example above this would be 10. print the a 10 times
                         for (int digit = 0; digit < Convert.ToInt32(encodeTimes); digit++)
@@ -78,27 +81,36 @@ namespace TestProject
                             result = result + expandEncoded;
                         }
 
+                        timesToDuplicate = "";
                         inBracketCollecting = false;
                     }
                 }
-
+                  
                 // collect characters from no collecting to pass part
                 // need to remove the numbers
                 if (!inBracketCollecting && cVal != sP && cVal != eP)
                 {
-                    // exclude digit[
+                    bool skip = false;
+
+                    // don't include the numbers that are used for encoding.
                     if (char.IsDigit(cVal))
                     {
-                        bool skip = false;
                         int lookAhead = currIndex;
+
+                        // while there are a string of numbers up to the point of a sP: 29[ the (2; current vale should be skipped)
                         while (char.IsDigit(encoded[lookAhead]))
                         {
+                            timesToDuplicate = timesToDuplicate + encoded[lookAhead];
                             lookAhead++;
                         }
 
+                        // all numbers up to the sp.
                         if(encoded[lookAhead] == sP)
                         {
                             skip = true;
+
+                            // keep for loop adds one to index
+                            currIndex = lookAhead -1;
                         }
                     }
 
